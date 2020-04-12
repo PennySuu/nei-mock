64f4c63ec5aa5da7a792d17eee57520f0f9d396e
sujun <sujun2015@126.com>
2020-04-12 22:26:52 +0800

    fix

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
 'use strict'
 
-async function start(app, config = {}) {
-  const {build, generateRouterMap} = require('./lib/nei')
-  await build(config.keys)
-  generateRouterMap()
-  if (config.online) {
-    app.use(require('./lib/online.js'))
-  } else {
-    app.use(require('./lib/local.js'))
-  }
-}
-
 function Mock(app, config = {}) {
   if (!config.keys.length) {
     console.log('mock-nei error: project key is required')
     return
   }
-  global.config = config
+  global.mock_nei_config = config
   if (app) {
-    start(app, config)
-      .catch(function (error) {
-        console.log('promise error', error)
-      })
-  } else {
-    if (config.online) {
-      return require('./lib/online.js')
-    } else {
-      return require('./lib/local.js')
-    }
+    app.use(require(`./lib/${config.online ? 'online' : 'local'}.js`))
+  }else{
+    return require(`./lib/${config.online ? 'online' : 'local'}.js`)
   }
-
 }
 
 module.exports = Mock
